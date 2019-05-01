ef50968f25f4b2f591fb6a6660e88a01dfbee0ef
User Fujirecords <fujirecords@www679.sakura.ne.jp>
2019-05-01 11:35:04 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,232 @@
+<!doctype html>
+<html ⚡>
+  <head>
+    <meta charset="utf-8">
+    <title>FUJI Records</title>
+    <link rel="canonical" href="https://fujirecords.com/">
+    <link rel="manifest" href="/manifest.json">
+    <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1">
+    <style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style><noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>
+    <script type="application/ld+json">
+    {
+      "@context": "https://schema.org",
+      "@type": "PerformingGroup",
+      "url": "https://fujirecords.com/?",
+      "name": "FUJI Records"
+    }
+    </script>
+    <script async src="https://cdn.ampproject.org/v0.js"></script>
+    <script async custom-element="amp-video" src="https://cdn.ampproject.org/v0/amp-video-0.1.js"></script>
+    <script async custom-element="amp-story" src="https://cdn.ampproject.org/v0/amp-story-1.0.js"></script>
+    <!--<script async custom-element="amp-sidebar" src="https://cdn.ampproject.org/v0/amp-sidebar-0.1.js"></script>-->
+    <link href="https://fonts.googleapis.com/css?family=Oswald:200,300,400" rel="stylesheet">
+    <style amp-custom>
+      amp-story {
+        font-family: 'Oswald',sans-serif;
+        color: #fff;
+      }
+      amp-story-page {
+        background-color: #000;
+      }
+      h1 {
+        font-weight: bold;
+        font-size: 2.875em;
+        font-weight: normal;
+        line-height: 1.174;
+      }
+      p {
+        font-weight: normal;
+        font-size: 1.3em;
+        line-height: 1.5em;
+        color: #fff;
+      }
+      q {
+        font-weight: 300;
+        font-size: 1.1em;
+      }
+      amp-story-grid-layer.bottom {
+        align-content:end;
+      }
+      amp-story-grid-layer.noedge {
+        padding: 0px;
+      }
+      amp-story-grid-layer.center-text {
+        align-content: center;
+      }
+      .wrapper {
+        display: grid;
+        grid-template-columns: 50% 50%;
+        grid-template-rows: 50% 50%;
+      }
+      .banner-text {
+        text-align: center;
+        background-color: #000;
+        line-height: 2em;
+      }
+    </style>
+    <!-- AMP Analytics -->
+    <script async custom-element="amp-analytics" src="https://cdn.ampproject.org/v0/amp-analytics-0.1.js"></script>
+    <script async custom-element="amp-install-serviceworker" src="https://cdn.ampproject.org/v0/amp-install-serviceworker-0.1.js"></script>
+  </head>
+  <body>
+    <!-- Cover page -->
+    <amp-story standalone supports-landscape
+        title="Welcome to FUJI Records"
+        publisher="AMP tutorials"
+        publisher-logo-src="assets/AMP-Brand-White-Icon.svg"
+        poster-portrait-src="assets/cover.jpg">
+
+      <!-- Google Tag Manager -->
+      <amp-analytics config="https://www.googletagmanager.com/amp.json?id=GTM-5Q53MHB" data-credentials="include"></amp-analytics>
+
+      <amp-story-page id="cover">
+        <amp-story-grid-layer template="fill">
+          <amp-img src="assets/cover.jpg"
+            width="1200" height="440"
+            layout="responsive"
+            object-position="40% 50%">
+          </amp-img>
+        </amp-story-grid-layer>
+        <amp-story-grid-layer template="vertical">
+          <h1>Welcome to<br>FUJI Records</h1>
+          <!--<p>By AMP Tutorials</p>-->
+
+          <!-- ServiceWorker -->
+          <amp-install-serviceworker
+            src="https://fujirecords.com/serviceworker.js"
+            layout="nodisplay">
+          </amp-install-serviceworker>
+
+        </amp-story-grid-layer>
+      </amp-story-page>
+
+      <amp-story-page id="page1">
+        <amp-story-grid-layer template="fill">
+          <amp-img src="assets/01.jpg"
+              width="1200" height="440"
+              layout="responsive"
+              object-position="75% 50%">
+          </amp-img>
+        </amp-story-grid-layer>
+          <amp-story-grid-layer template="thirds">
+            <h1 grid-area="upper-third"></h1>
+            <p grid-area="lower-third">
+              Noriaki Mori Large Ensemble<br>
+              Live at Akihabara TokyoTUC<br>
+              2018.4.29
+            </p>
+        </amp-story-grid-layer>
+      </amp-story-page>
+
+      <amp-story-page id="page2">
+        <amp-story-grid-layer template="fill">
+          <amp-img src="assets/02.jpg"
+              width="1200" height="440"
+              layout="responsive"
+              object-position="75% 50%">
+          </amp-img>
+        </amp-story-grid-layer>
+          <amp-story-grid-layer template="thirds">
+            <h1 grid-area="upper-third"></h1>
+            <p grid-area="lower-third">
+              Tetsuya Tatsumi Special Tentet<br>
+              Holiday Afternoon Jazz Live #12<br>
+              Bob Florence Decennial
+              <br>Live at Akihabara TokyoTUC
+              <br>2018.4.30
+            </p>
+        </amp-story-grid-layer>
+      </amp-story-page>
+
+      <amp-story-page id="page3">
+        <amp-story-grid-layer template="fill">
+          <amp-img src="assets/03.jpg"
+              width="1200" height="440"
+              layout="responsive">
+          </amp-img>
+        </amp-story-grid-layer>
+          <amp-story-grid-layer template="thirds">
+            <h1 grid-area="upper-third"></h1>
+            <p grid-area="lower-third">
+              野口久和 THE BIG BAND with “BREEZE”<br>
+              Live at Akihabara TokyoTUC<br>
+              2018.5.3<br>
+            </p>
+        </amp-story-grid-layer>
+      </amp-story-page>
+
+      <amp-story-page id="page4">
+        <amp-story-grid-layer template="fill">
+          <amp-img src="assets/04.jpg"
+              width="1200" height="440"
+              layout="responsive">
+          </amp-img>
+        </amp-story-grid-layer>
+          <amp-story-grid-layer template="thirds">
+            <h1 grid-area="upper-third"></h1>
+            <p grid-area="lower-third">
+              Sara McDonald with Keio Light Music Society<br>
+              Live at Space Do<br>
+              2018.10.29
+            </p>
+        </amp-story-grid-layer>
+      </amp-story-page>
+
+      <amp-story-page id="page5">
+        <amp-story-grid-layer template="fill">
+          <amp-img src="assets/05.jpg"
+              width="1200" height="440"
+              layout="responsive">
+          </amp-img>
+        </amp-story-grid-layer>
+          <amp-story-grid-layer template="thirds">
+            <h1 grid-area="upper-third"></h1>
+            <p grid-area="lower-third">
+              山木幸三郎を送る会<br>
+              Live at Space Do<br>
+              2019.1.26
+            </p>
+        </amp-story-grid-layer>
+      </amp-story-page>
+
+      <amp-story-page id="page6">
+        <amp-story-grid-layer template="fill">
+          <amp-img src="assets/06.jpg"
+              width="1200" height="440"
+              layout="responsive"
+              object-position="75% 50%">
+          </amp-img>
+        </amp-story-grid-layer>
+          <amp-story-grid-layer template="thirds">
+            <h1 grid-area="upper-third"></h1>
+            <p grid-area="lower-third">
+              Noriaki Mori Large Ensemble<br>
+              feat. ZINKandSILENTLISTEN<br>
+              Live at Koen-dori Classics<br>
+              2019.2.11</p>
+        </amp-story-grid-layer>
+      </amp-story-page>
+
+      <amp-story-page id="page7">
+        <amp-story-grid-layer template="fill">
+          <amp-img src="assets/07.jpg"
+              width="1200" height="440"
+              layout="responsive"
+              object-position="75% 50%">
+          </amp-img>
+        </amp-story-grid-layer>
+          <amp-story-grid-layer template="thirds">
+            <h1 grid-area="upper-third"></h1>
+            <p grid-area="lower-third">
+              Noriaki Mori Large Ensemble<br>
+              feat. ZINKandSILENTLISTEN<br>
+              Live at Koen-dori Classics<br>
+              2019.2.11</p>
+        </amp-story-grid-layer>
+      </amp-story-page>
+
+      <!-- Bookend -->
+      <amp-story-bookend src="bookend.json" layout="nodisplay"></amp-story-bookend>
+    </amp-story>
+  </body>
+</html>
---
+++ serviceworker.js
@@ -0,0 +1,4 @@
+
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.3.1/workbox-sw.js');
+
+self.addEventListener('fetch', function(event) {});
