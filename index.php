bf805f51e7721e6b1ff4ccad915b508ef0bf38cd
冨士孔太 <>
2019-05-01 20:02:22 +0900

    change file pass

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <title>FUJI Records</title>
     <link rel="canonical" href="https://fujirecords.com/">
-    <link rel="manifest" href="/manifest.json">
+    <link rel="manifest" href="https://fujirecords.com/manifest.json">
     <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1">
     <style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style><noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>
     <script type="application/ld+json">
